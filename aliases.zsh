af434719fe3176d16a8b68d72753e80735bc3acb
Taras Krutiak <tkrutiak@proffiz.com>
2018-06-26 16:11:03 +0300

    initial commit

---
+++ aliases.zsh
@@ -0,0 +1,9 @@
+#Maven
+alias mci="mvn clean install"
+alias mst="mvn clean install -DskipTests"
+alias mdt="mvn dependency:tree"
+#GIT
+alias gc-="git checkout -"
+#cd
+alias cdi="cd ~/IdeaProjects"
+alias S="sudo"
